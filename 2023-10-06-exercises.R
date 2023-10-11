8a6bb5cc72044ed59c43b96770381207d3970ed4
Colin Rundel <rundel@gmail.com>
2023-10-11 10:00:07 -0400

    Clqss

---
+++ 2023-10-06-exercises.R
@@ -0,0 +1,43 @@
+library(stringr)
+
+
+## Exercise 1
+
+x = c("Jeremy Cruz", "Nathaniel Le", "Jasmine Chu", "Bradley Calderon Raygoza", 
+      "Quinten Weller", "Katelien Kanamu-Hauanio", "Zuhriyaa al-Amen", 
+      "Travale York", "Alexis Ahmed", "David Alcocer", "Jairo Martinez", 
+      "Dwone Gallegos", "Amanda Sherwood", "Hadiyya el-Eid", "Shaimaaa al-Can", 
+      "Sarah Love", "Shelby Villano", "Sundus al-Hashmi", "Dyani Loving", 
+      "Shanelle Douglas")
+
+## detects if the person’s first name starts with a vowel (a,e,i,o,u)
+
+str_subset(x, "^[AEIOU]")
+
+## detects if the person’s last name starts with a vowel
+
+str_subset(x, " [AEIOUaeiou].*$")
+
+str_subset(x, regex(" [aeiou].*$", ignore_case = TRUE))
+
+## detects if either the person’s first or last name start with a vowel
+
+str_subset(x, "^[AEIOU]| [AEIOUaeiou].*$")
+
+## detects if neither the person’s first nor last name start with a vowel
+
+x[!str_detect(x, "^[AEIOU]") & !str_detect(x, " [AEIOUaeiou].*$")]
+
+
+## Exercise 2
+
+text = c(
+  "apple" , 
+  "219 733 8965" , 
+  "329-293-8753" ,
+  "Work: (579) 499-7527; Home: (543) 355 3679"
+)
+
+str_view_all(
+  text, "\\(?(\\d{3})\\)?[- ](\\d{3})[- ](\\d{4})"
+)
